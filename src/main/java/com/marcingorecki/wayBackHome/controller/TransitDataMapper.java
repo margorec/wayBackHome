6a0d717878dea5b1be717f83fbc2a54c1071cdfd
Marcin Górecki <margorec@gmail.com>
2018-04-01 17:14:31 +0200

    Commit to show earlier version

--- src/main/java/com/marcingorecki/wayBackHome/controller/TransitDataMapper.java
+++ src/main/java/com/marcingorecki/wayBackHome/controller/TransitDataMapper.java
@@ -3,17 +3,29 @@ package com.marcingorecki.wayBackHome.controller;
 import com.marcingorecki.wayBackHome.model.application.Ride;
 import com.marcingorecki.wayBackHome.model.application.TransitData;
 import com.marcingorecki.wayBackHome.model.dto.Data;
+import com.marcingorecki.wayBackHome.model.dto.Delay;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 @Component
 public class TransitDataMapper {
 
     public TransitData map(Data data) {
-        return new TransitData(
-                new Ride.Builder().withDelayInSeconds(data.getDelay())
-        )
+        List<Ride> incomingRides = data.getDelay()
+                .stream()
+                .map(mapDelayToRide)
+                .collect(Collectors.toList());
 
-
-        return null;
+        return new TransitData(null, null, incomingRides);
     }
+
+    Function<Delay, Ride> mapDelayToRide = r -> new Ride.Builder()
+            .withDelayInSeconds(r.getDelayInSeconds())
+            .withTheoreticalTime(r.getTheoreticalTime())
+            .withEstimatedTime(r.getEstimatedTime())
+            .withHeadsign(r.getHeadsign())
+            .build();
 }
